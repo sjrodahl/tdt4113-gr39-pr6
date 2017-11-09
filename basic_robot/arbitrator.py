77f4d9fd3a559aed48d3f4e3a825fdfc2889dce5
sjrodahl <son.rodahl@gmail.com>
2017-11-09 11:35:23 +0100

    Added bbcon and arbitrator

---
+++ basic_robot/arbitrator.py
@@ -0,0 +1,9 @@
+class Arbitrator:
+    def __init__(self, behaviors):
+        self.behaviors = behaviors
+
+    def choose_action(self):
+        """Choose behaviour based on their weights"""
+        #TODO: Add stochasticity (weighted randomness)
+        chosen = max(self.behaviors)
+        return (chosen.motor_recommendations, chosen.halt_request)
