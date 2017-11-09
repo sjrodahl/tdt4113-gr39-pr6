176bf62ef6259c3cfc6d86b0c0a3ddf1f35feb8b
sjrodahl <son.rodahl@gmail.com>
2017-11-09 17:45:29 +0100

    Added Camera, Ultra and IR-sensob

---
+++ basic_robot/sensobs.py
@@ -0,0 +1,103 @@
+from PIL import Image
+from camera import Camera
+from ultrasonic import Ultrasonic
+from irproximity_sensor import IRProximitySensor
+
+
+
+class Sensob():
+
+    def update(self):
+        pass
+
+    def reset(self):
+        pass
+
+
+
+class RedandBlue_Sensob(Sensob):
+
+
+    def __init__(self):
+        #self.image = Image.open("redandblue.jpg")
+        self.camera = Camera()
+        self.image = Camera.get_value()
+
+    def get_pixel(self,x,y):
+        return self.image.getpixel((x,y))
+
+    def put_pixel(self,x,y):
+        Black = (0, 0, 0)
+        return self.image.putpixel((x,y),Black)
+
+    def Converter(self):
+
+        for x in range(self.image.size[0]):
+            for y in range(self.image.size[1]):
+                #print((self.get_pixel(x,y))[1:])
+                # placing between 0 and 80 so that red with some small variationes are not removed
+                if (self.get_pixel(x,y)[1] >=(0)) and (self.get_pixel(x,y)[1] <=(80)):
+                    pass
+                else:
+                    self.put_pixel(x,y)
+
+        #self.image.show()
+
+    def Array(self):
+        self.content = []
+
+        for x in range(self.image.size[0]):
+            temp = [0,0]
+            for y in range(self.image.size[1]):
+                temp[0] += self.get_pixel(x,y)[0]
+                temp[1] += self.get_pixel(x,y)[2]
+
+            if (temp[0] > 2000) or (temp[1] > 2000):
+                if(temp[0] > temp[1]):
+                    self.content.append("Red")
+                else:
+                    self.content.append("Blue")
+            else:
+                self.content.append(None)
+
+        #print(self.content)
+
+    def update(self):
+        self.image = self.camera.update()
+        self.Converter()
+        self.Array()
+        return self.content
+
+    def reset(self):
+        self.camera.reset()
+
+
+class UltrasonicSensob(Sensob):
+
+    def __init__(self):
+        self.ultra = Ultrasonic()
+        self.distance = 0
+
+    def update(self):
+        self.distance = self.ultra.update()
+
+    def reset(self):
+        self.ultra.reset()
+
+class IrSensob(Sensob):
+
+    def __init__(self):
+        self.ir = IRProximitySensor()
+        self.is_close = [False, False]
+
+    def update(self):
+        self.is_close = self.ir.update()
+
+    def reset(self):
+        self.ir.reset()
+
+    def get_value(self):
+        return self.is_close
+
+
+
